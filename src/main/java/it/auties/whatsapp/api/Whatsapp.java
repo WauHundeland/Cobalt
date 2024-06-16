24cd5ab6c56502914490002432ba8ae1b6614b80
Quishot WADEV <70950705+Darker935@users.noreply.github.com>
2024-06-16 10:06:51 -0300

    Invoking randomIdV2()

--- src/main/java/it/auties/whatsapp/api/Whatsapp.java
+++ src/main/java/it/auties/whatsapp/api/Whatsapp.java
@@ -413,7 +413,7 @@ public class Whatsapp {
      */
     public CompletableFuture<? extends MessageInfo> sendReaction(MessageInfo message, String reaction) {
         var key = new ChatMessageKeyBuilder()
-                .id(ChatMessageKey.randomId())
+                .id(ChatMessageKey.randomIdV2(message.senderJid(), store().clientType()))
                 .chatJid(message.parentJid())
                 .senderJid(message.senderJid())
                 .fromMe(Objects.equals(message.senderJid().toSimpleJid(), jidOrThrowError().toSimpleJid()))
@@ -573,7 +573,7 @@ public class Whatsapp {
                 .deviceListMetadataVersion(2)
                 .build();
         var key = new ChatMessageKeyBuilder()
-                .id(ChatMessageKey.randomId())
+                .id(ChatMessageKey.randomIdV2(jidOrThrowError(), store().clientType()))
                 .chatJid(recipient.toJid())
                 .fromMe(true)
                 .senderJid(jidOrThrowError())
@@ -687,7 +687,7 @@ public class Whatsapp {
                 .deviceListMetadataVersion(2)
                 .build();
         var key = new ChatMessageKeyBuilder()
-                .id(ChatMessageKey.randomId())
+                .id(ChatMessageKey.randomIdV2(jidOrThrowError(), store().clientType()))
                 .chatJid(Jid.of("status@broadcast"))
                 .fromMe(true)
                 .senderJid(jidOrThrowError())
@@ -1730,7 +1730,7 @@ public class Whatsapp {
                     .build();
             var sender = messageInfo.chatJid().hasServer(JidServer.GROUP) ? jidOrThrowError() : null;
             var key = new ChatMessageKeyBuilder()
-                    .id(ChatMessageKey.randomId())
+                    .id(ChatMessageKey.randomIdV2(sender, store().clientType()))
                     .chatJid(messageInfo.chatJid())
                     .fromMe(true)
                     .senderJid(sender)
